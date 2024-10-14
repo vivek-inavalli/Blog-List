973a9c4f6186bd6fbef1a5605efaa671545c6be0
vivek <inavallivivek6@gmail.com>
2024-10-14 06:39:30 +0000

    first

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const UserInfo = () => {
+  return (
+    <div className="imgContainer">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+        alt="profile"
+        className="img"
+      />
+      <p>Wade Warren</p>
+      <p>Software developer at UK</p>
+    </div>
+  )
+}
+
+export default UserInfo
